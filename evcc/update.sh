19bd7d2a04be06b20484cf9f01ce330591559100
tm-107 <41205746+tm-107@users.noreply.github.com>
2025-03-26 11:46:47 +0100

    Update update.sh

--- evcc/update.sh
+++ evcc/update.sh
@@ -1,32 +1,39 @@
 #!/bin/sh
 
-# For security reasons you have to make the script executable first.
-# (chmod +x update.sh)
-
 # Add the needed version here:
 
-VERSION=x.xxx.x
+Version=0.202.0
 
 # No need to edit after this line
 
-if [ "$VERSION" = "x.xxx.x" ]; then
-  echo "You have to edit the file first !!!"
-  exit 1
+
+if [ "$Version" = "x.xxx.x" ]; then
+        echo "You have to edit the file first !!!"
+        exit 1
 fi
 
-cd /data/evcc # just for safety, should not be needed because script should be located there
+cd /data/evcc # just for safety, should not be needed
 echo "stopping evcc ..."
 ./down
 mkdir evcc-update
 cd evcc-update
-echo "downloading evcc v$VERSION ..."
-wget "https://github.com/evcc-io/evcc/releases/download/$VERSION/evcc_"$VERSION"_linux-armv6.tar.gz"
-echo "unpacking evcc v$VERSION ..."
-tar xzf "evcc_"$VERSION"_linux-armv6.tar.gz"
-echo "copying evcc v$VERSION ..."
+echo "downloading new evcc v$Version ..."
+wget "https://github.com/evcc-io/evcc/releases/download/$Version/evcc_"$Version"_linux-armv6.tar.gz"
+if ! test -f "/data/evcc/evcc-update/evcc_"$Version"_linux-armv6.tar.gz"; then
+        echo "Download fehlgeschlagen!"
+        echo "delete downloaded files ..."
+        cd ..
+        rm -r evcc-update
+        echo "starting previous evcc-version ..."
+        ./up
+        exit 1
+fi
+echo "unpacking evcc v$Version ..."
+tar xzf "evcc_"$Version"_linux-armv6.tar.gz"
+echo "copying evcc v$Version ..."
 cp evcc /data/evcc/
-cd ..
 echo "delete downloaded files ..."
+cd ..
 rm -r evcc-update
-echo "starting evcc v$VERSION ..."
+echo "starting evcc v$Version ..."
 ./up
