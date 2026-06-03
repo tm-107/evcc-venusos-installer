a1c2895398566e64ee79f278fd23ee3e2881938f
tm-107 <41205746+tm-107@users.noreply.github.com>
2026-06-03 08:49:50 +0200

    Update update.sh
    Add a few checks and backup of database

--- evcc/update.sh
+++ evcc/update.sh
@@ -13,12 +13,34 @@ if [ "$Version" = "x.xxx.x" ]; then
 fi
 
 cd /data/evcc # just for safety, should not be needed
+if ! [ "$?" = 0 ]; then
+        echo "Fehler: EVCC-Verzeichnis nicht vorhanden?"
+        exit 1
+fi
+
 echo "stopping evcc ..."
 ./down
 mkdir evcc-update
+if ! [ "$?" = 0 ]; then
+        echo "Fehler beim Erstellen des temporaeren Verzeichnis"
+        exit 1
+fi
 cd evcc-update
+if ! [ "$?" = 0 ]; then
+        echo "Temporaeres Verzeichnis nicht gefunden"
+        exit 1
+fi
 echo "downloading new evcc v$Version ..."
 wget "https://github.com/evcc-io/evcc/releases/download/$Version/evcc_"$Version"_linux-armv6.tar.gz"
+if ! [ "$?" = 0 ]; then
+        echo "Error downloading new version"
+        echo "delete temporary files ..."
+        cd ..
+        rm -r evcc-update
+        echo "starting previous evcc-version ..."
+        ./up
+        exit 1
+fi
 
 if ! test -f "/data/evcc/evcc-update/evcc_"$Version"_linux-armv6.tar.gz"; then
         echo "Download fehlgeschlagen!"
@@ -32,8 +54,31 @@ fi
 
 echo "unpacking evcc v$Version ..."
 tar xzf "evcc_"$Version"_linux-armv6.tar.gz"
+if ! [ "$?" = 0 ]; then
+        echo "Error unpacking new version"
+        echo "delete temporary files ..."
+        cd ..
+        rm -r evcc-update
+        echo "starting previous evcc-version ..."
+        ./up
+        exit 1
+fi
+
+echo "backup database ..."
+cp /data/evcc/evcc.db /data/evcc/evcc.db_backup
+
 echo "copying evcc v$Version ..."
 cp evcc /data/evcc/
+if ! [ "$?" = 0 ]; then
+        echo "Error copying new version"
+        echo "delete temporary files ..."
+        cd ..
+        rm -r evcc-update
+        echo "starting previous evcc-version ..."
+        ./up
+        exit 1
+fi
+
 echo "delete downloaded files ..."
 cd ..
 rm -r evcc-update
